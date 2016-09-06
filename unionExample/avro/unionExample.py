c5c7586d763a97dfcd58037853a061482287df54
A. Vicki Crosson <amber.crosson@nytimes.com>
2016-09-06 13:20:37 -0400

    Removed unnecessary code in unionExample

--- unionExample/avro/unionExample.py
+++ unionExample/avro/unionExample.py
@@ -6,17 +6,3 @@ from avro.io import DatumReader, DatumWriter
 
 #Read in the helloWorld schema from the JSON
 schema = avro.protocol.parse(open("unionExample.avpr", "rb").read())
-
-#To use the IDL file, compile first using maven plugin
-
-writer = DataFileWriter(open("output.avro", "wb"), DatumWriter(), schema)
-
-writer.append({"username": "alyssaPH", "favorite_nums" : [1,1,2,3,5,7], "opt_favorite_color": "lilac"})
-
-writer.close()
-
-reader = DataFileReader(open("output.avro", "rb"), DatumReader())
-
-for user in reader:
-    print user
-reader.close()
